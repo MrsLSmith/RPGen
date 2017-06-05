dbac727f7ee048f245a1a4cf3d1f1acc53442827
samtheecuadorian <svelasquez@ccsuvt.org>
2017-06-05 12:59:19 -0400

    attempts were made
    Attempt to link emails into firebase database and created a function in which retrieves data (hopefully)

--- public/js/event.js
+++ public/js/event.js
@@ -7,9 +7,9 @@ function googleSignin() {
    .signInWithPopup(provider).then(function(result) {
       var token = result.credential.accessToken;
       var user = result.user;
-
-      console.log(token)
-      console.log(user)
+      console.log(token);
+      console.log(user);
+      console.log(typeof user.email);
    }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -17,6 +17,7 @@ function googleSignin() {
       console.log(error.code)
       console.log(error.message)
    });
+
 }
 
 function googleSignout() {
@@ -54,18 +55,40 @@ function save() {
                  document.getElementById('wis').value,
                  document.getElementById('cha').value],
     modifiers: ["1:strMOD 2:dexMOD 3:conMOD 4:intMOD 5:wisMOD 6:chaMOD",
-                document.getElementById('strMod').innerHTML,
-                document.getElementById('dexMod').innerHTML,
-                document.getElementById('conMod').innerHTML,
-                document.getElementById('intMod').innerHTML,
-                document.getElementById('wisMod').innerHTML,
-                document.getElementById('chaMod').innerHTML]
+                document.getElementById('strMod').textContent,
+                document.getElementById('dexMod').textContent,
+                document.getElementById('conMod').textContent,
+                document.getElementById('intMod').textContent,
+                document.getElementById('wisMod').textContent,
+                document.getElementById('chaMod').textContent],
 
-    //classInfo: " ",        //determined based on class
+    classInfo: {
+      hp: document.getElementById('hitPoints').textContent,
+      fortClass: document.getElementById('fortClass').textContent,
+      fortMod: document.getElementById('fortMod').textContent,
+      fortTotal: document.getElementById('fortTotal').textContent,
+      refClass: document.getElementById('refClass').textContent,
+      refMod: document.getElementById('refMod').textContent,
+      refTotal: document.getElementById('refTotal').textContent,
+      wilClass: document.getElementById('wilClass').textContent,
+      wilMod: document.getElementById('wilMod').textContent,
+      wilTotal: document.getElementById('wilTotal').textContent,
+    },        //determined based on class
   };
-
   var firebaseRef = firebase.database().ref();
+
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    var currentUser = user.displayName;
+    console.log(currentUser);
+  } else {
+    console.log("no user detected");
+  }
+  });
+
+
   firebaseRef.push().set({
+    name: currentUser.toString(),
     character: character
   });
   pdf(character);
@@ -78,32 +101,92 @@ function pdf(char) {
   doc.setFontSize(30);
   doc.text(100, 20, 'Pathfinder character sheet', null, null, 'center');
 
+  //---general information---\\
   doc.setFontSize(10);
   doc.text(20, 30, 'character name: ' + char.name);
   doc.text(20, 40, 'race: ' + char.race);
   doc.text(20, 50, 'class: ' + char.charClass);
   doc.text(20, 60, 'alignment: ' + char.alignment);
 
-  //--table rectangles--\\
+  //--table lines--\\
+
+  //outer lines
+  doc.line(25, 85, 115, 85);
+  doc.line(25, 85, 25, 152);
+  doc.line(55, 85, 55, 152);
+  doc.line(85, 85, 85, 152);
+  doc.line(115, 85, 115, 152);
+  doc.line(25, 152, 115, 152);
 
 
+  //inner lines
+  doc.line(25, 94, 115, 94);
+  doc.line(25, 104, 115, 104);
+  doc.line(25, 114, 115, 114);
+  doc.line(25, 123, 115, 123);
+  doc.line(25, 134, 115, 134);
+  doc.line(25, 143, 115, 143);
 
-  //---table content---\\
+  //---table one content---\\
+
   doc.text(40, 90, 'Ability', null, null, 'center');
-    doc.text(80, 90, 'Ability Score', null, null, 'center');
-      doc.text(120, 90, 'Ability Modifier', null, null, 'center');
+    doc.text(70, 90, 'Ability Score', null, null, 'center');
+      doc.text(100, 90, 'Ability Modifier', null, null, 'center');
   doc.text(40, 100, 'Strength', null, null, 'center');
-    doc.text(80, 100, char.attributes[1], null, null, 'center');
+    doc.text(70, 100, ' ' + char.attributes[1], null, null, 'center');
+      doc.text(100, 100, ' ' + char.modifiers[1], null, null, 'center');
   doc.text(40, 110, 'Dexterity', null, null, 'center');
-
+    doc.text(70, 110, ' ' + char.attributes[2], null, null, 'center');
+      doc.text(100, 110, ' ' + char.modifiers[2], null, null, 'center');
   doc.text(40, 120, 'Constitution', null, null, 'center');
-
+    doc.text(70, 120, ' ' + char.attributes[3], null, null, 'center');
+      doc.text(100, 120, ' ' + char.modifiers[3], null, null, 'center');
   doc.text(40, 130, 'Intelligence', null, null, 'center');
-
+    doc.text(70, 130, ' ' + char.attributes[4], null, null, 'center');
+      doc.text(100, 130, ' ' + char.modifiers[4], null, null, 'center');
   doc.text(40, 140, 'Wisdom', null, null, 'center');
-
+    doc.text(70, 140, ' ' + char.attributes[5], null, null, 'center');
+      doc.text(100, 140, ' ' + char.modifiers[5], null, null, 'center');
   doc.text(40, 150, 'Charisma', null, null, 'center');
+    doc.text(70, 150, ' ' + char.attributes[6], null, null, 'center');
+      doc.text(100, 150, ' ' + char.modifiers[6], null, null, 'center');
+
+  //---table two content---\\
+
+doc.text(30, 170, 'Hit Points: ' + char.classInfo.hp, null, null, 'center');
+  doc.text(40, 180, 'Save', null, null, 'center');
+    doc.text(70, 180, 'Class', null, null, 'center');
+      doc.text(100, 180, 'Modifier', null, null, 'center');
+        doc.text(130, 180, 'Total', null, null, 'center');
+  doc.text(40, 190, 'Fortitude', null, null, 'center');
+    doc.text(70, 190, char.classInfo.fortClass, null, null, 'center');
+      doc.text(100, 190, char.classInfo.fortMod, null, null, 'center');
+        doc.text(130, 190, char.classInfo.fortTotal, null, null, 'center');
+  doc.text(40, 200, 'Reflex', null, null, 'center');
+    doc.text(70, 200, char.classInfo.refClass, null, null, 'center');
+      doc.text(100, 200, char.classInfo.refMod, null, null, 'center');
+        doc.text(130, 200, char.classInfo.refTotal, null, null, 'center');
+  doc.text(40, 210, 'Will', null, null, 'center');
+    doc.text(70, 210, char.classInfo.wilClass, null, null, 'center');
+      doc.text(100, 210, char.classInfo.wilMod, null, null, 'center');
+        doc.text(130, 210, char.classInfo.wilTotal, null, null, 'center');
 
   doc.output("dataurlnewwindow");
 }
-//http://rawgit.com/MrRio/jsPDF/master/
+  //http://rawgit.com/MrRio/jsPDF/master/
+
+function retrive() {
+  // Import Admin SDK
+  var admin = require("firebase-admin");
+
+  // Get a database reference to our posts
+  var db = admin.database();
+  var ref = db.ref("server/saving-data/fireblog/posts");
+
+  // Attach an asynchronous callback to read the data at our posts reference
+  ref.on("value", function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+}
